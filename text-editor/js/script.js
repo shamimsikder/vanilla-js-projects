9da93ff435342d9dadf9bf86e8c1e0c2c8859473
shamimsikder <shamimsikder138@gmail.com>
2023-02-16 11:14:36 +0600

    Text Style JS

---
+++ text-editor/js/script.js
@@ -0,0 +1,5 @@
+// Text Style
+
+document.getElementById('bold-btn').addEventListener('click', boldFontStyle)
+document.getElementById('italic-btn').addEventListener('click', italicFontStyle)
+document.getElementById('underline-btn').addEventListener('click', underlineFontStyle)
---
+++ text-editor/js/text.js
@@ -0,0 +1,27 @@
+// Function for Bold Text
+function boldFontStyle(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.fontWeight = 'bold'
+
+}
+
+// Function for Italic Text
+function italicFontStyle(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.fontStyle = 'italic'
+
+}
+
+// Function for Text Underline
+function underlineFontStyle(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.textDecoration = 'underline'
+
+}
+
