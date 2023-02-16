1f3ca3e3a058d5ee555e96f69795bcd820b595d1
shamimsikder <shamimsikder138@gmail.com>
2023-02-16 11:20:58 +0600

    Text Align JS

--- text-editor/js/script.js
+++ text-editor/js/script.js
@@ -3,3 +3,8 @@
 document.getElementById('bold-btn').addEventListener('click', boldFontStyle)
 document.getElementById('italic-btn').addEventListener('click', italicFontStyle)
 document.getElementById('underline-btn').addEventListener('click', underlineFontStyle)
+
+// Text Align
+document.getElementById('left-align-btn').addEventListener('click', leftTextAlign)
+document.getElementById('center-align-btn').addEventListener('click', centerTextAlign)
+document.getElementById('right-align-btn').addEventListener('click', rightTextAlign)
---
+++ text-editor/js/text-align.js
@@ -0,0 +1,26 @@
+// Function for Left Text Align
+function leftTextAlign(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.textAlign = 'left'
+
+}
+
+// Function for Center Text Align
+function centerTextAlign(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.textAlign = 'center'
+
+}
+
+// Function for Right Text Align
+function rightTextAlign(){
+
+    const text = document.getElementById('input-field')
+
+    text.style.textAlign = 'right'
+
+}
