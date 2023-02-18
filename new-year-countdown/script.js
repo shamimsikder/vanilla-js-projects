5b64569de9c1a7b3874dff0432c067fe2797c9b5
shamimsikder <shamimsikder138@gmail.com>
2023-02-18 23:38:28 +0600

    added id

---
+++ new-year-countdown/script.js
@@ -0,0 +1,7 @@
+const years = document.getElementById('years')
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+const countdown = document.getElementById('countdown')
+const loading = document.getElementById('loading')
