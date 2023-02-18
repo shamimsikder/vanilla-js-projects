d735e495b1a5f051801a08a53d9f071eafc1423c
shamimsikder <shamimsikder138@gmail.com>
2023-02-19 00:28:55 +0600

    Complete New Year Countdown

--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -5,3 +5,31 @@ const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 const countdown = document.getElementById('countdown')
 const loading = document.getElementById('loading')
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`)
+
+function updateCountdown(){
+
+     const currentTime = new Date()
+     const diff = newYearTime - currentTime
+
+     const d = Math.floor(diff / 1000 / 60 / 60 / 24)
+     const h = Math.floor(diff / 1000 / 60 / 60) % 24
+     const m = Math.floor(diff / 1000 / 60) % 60
+     const s = Math.floor(diff / 1000) % 60
+
+    days.innerText = d
+    hours.innerText = h < 10 ? '0' + h : h
+    minutes.innerText = m < 10 ? '0' + m : m
+    seconds.innerText = s < 10 ? '0' + s : s
+
+}
+
+setTimeout(() => {
+    loading.remove()
+    countdown.classList.remove('hidden')
+},1000)
+
+setInterval(updateCountdown, 1000)
