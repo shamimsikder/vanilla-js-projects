451bed8c784c623a2c49aae0e59646990e323aef
shamimsikder <shamimsikder138@gmail.com>
2023-03-08 15:15:53 +0600

    Load & Show Weather Data

---
+++ weather-app/js/app.js
@@ -0,0 +1,41 @@
+const API_KEY = 'db8e45b7ea4d867aea193ac7b43d7b04';
+
+const loadWeatherData = async(cityName) => {
+
+    try{
+
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+
+        const res = await fetch(URL);
+        const data = await res.json();
+
+        showWeatherData(data)
+
+    }
+
+    catch(error){
+        console.log(error.message)
+    }
+
+}
+
+const showWeatherData = (data) => {
+
+    console.log(data)
+
+    const city = document.getElementById('city-name').innerText = data.name
+    const temp = document.getElementById('temp').innerText = (data.main.temp).toFixed(1)
+    const weather = document.getElementById('weather').innerText = data.weather[0].main
+
+}
+
+document.getElementById('city-btn').addEventListener('click', function(){
+
+    const city = document.getElementById('city-field');
+
+    loadWeatherData(city.value);
+    city.value = '';
+
+})
+
+loadWeatherData('Dhaka')
